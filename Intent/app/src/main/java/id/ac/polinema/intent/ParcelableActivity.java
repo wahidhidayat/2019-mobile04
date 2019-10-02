9e4f65c21ad52ec51fc2ed87804211d73d19f4f5
wahidhidayat <wahidhidayat3899@gmail.com>
2019-10-02 22:09:16 +0700

    ParcelableActivity

--- Intent/app/src/main/java/id/ac/polinema/intent/ParcelableActivity.java
+++ Intent/app/src/main/java/id/ac/polinema/intent/ParcelableActivity.java
@@ -3,13 +3,21 @@ package id.ac.polinema.intent;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.EditText;
 
 public class ParcelableActivity extends AppCompatActivity {
+
+    private EditText usernameInput ;
+    private EditText nameInput ;
+    private EditText ageInput ;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_parcelable);
+        usernameInput = findViewById(R.id.input_username);
+        nameInput = findViewById(R.id.input_name);
+        ageInput = findViewById(R.id.input_age);
     }
 
 }
